39eed8756f61a535d67b2599c2636e936d7b375e
Hyun-38 <1226johnny@gmail.com>
2026-04-03 02:05:57 +0900

    feat(#5) : UserRepository 생성

---
+++ src/main/java/PickitPickit/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package PickitPickit.user.repository;
+
+import PickitPickit.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
